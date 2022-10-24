c5f84aedba83242c42692a5917ab527679b85353
ridvansumset <ridvansumset@gmail.com>
2022-10-24 12:20:52 +0100

    base-txt updated

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -17,9 +17,11 @@ export default function Default() {
       <div className="min-h-screen bg-apple-web dark:bg-slate-800">
         <div className="flex justify-between py-4 mb-8 border-b border-b-black dark:border-b-white">
           <div className="pl-8">
-            <Link to={''} className="text-slate-900 dark:text-slate-300">Home</Link> |
-            <Link to={'tic-tac-toe'} className="text-slate-900 dark:text-slate-300">{' '}Tic Tac Toe</Link> |
-            <Link to={'computer'} className="text-slate-900 dark:text-slate-300">{' '}Computer</Link>
+            <Link to={''} className="base-txt">Home</Link>
+            <span className="base-txt">{' '}|{' '}</span>
+            <Link to={'tic-tac-toe'} className="base-txt">Tic Tac Toe</Link>
+            <span className="base-txt">{' '}|{' '}</span>
+            <Link to={'computer'} className="base-txt">Computer</Link>
           </div>
 
           <div className="pr-8">
@@ -29,7 +31,7 @@ export default function Default() {
                   <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clipRule="evenodd" />
                 </svg>
 
-                <span className="text-sm text-slate-900 dark:text-slate-300">{darkMode ? 'Light' : 'Dark'} Mode</span>
+                <span className="base-txt text-sm">{darkMode ? 'Light' : 'Dark'} Mode</span>
               </div>
             </button>
           </div>
--- src/views/Computer.tsx
+++ src/views/Computer.tsx
@@ -8,7 +8,7 @@ export default function Computer() {
     <div className="flex justify-center">
       <div className="grid grid-cols-2 gap-4 w-full md:w-1/2">
         {parts.map((part) => (
-          <Link key={`${part}`} to={`${part}`} className="base-txt-lg p-8 transition ease-in-out delay-150 bg-blue-500
+          <Link key={`${part}`} to={`${part}`} className="base-txt text-lg p-8 transition ease-in-out delay-150 bg-blue-500
           hover:-translate-y-1 hover:scale-10 hover:bg-indigo-500 duration-300">
             {part}
           </Link>
